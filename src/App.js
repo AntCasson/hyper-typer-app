c13ba765f032e6a6bdaf789dac2a3b714b1dd288
AntCasson <anthonyrcasson@gmail.com>
2022-01-25 14:43:23 +0100

    input verandert in P element

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ export default function App() {
     
     const [seconds, setSeconds] = useState(20)
       
-    function secChange(e) {
-        const {value} = e.target
-        setSeconds(value)
-    }
+    
     useEffect( ()=> {
         setTextToCheck(getRandomParagraph)
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -34,16 +31,41 @@ export default function App() {
     
     return (
         <main className="wrapper">
-            <a className="github" aria-label="Link to Anthony Casson GitHub"href="https://github.com/AntCasson"><span className="sr-only">Link to GitHub page Anthony Casson</span></a>
+            <a 
+                className="github" 
+                aria-label="Link to Anthony Casson GitHub"
+                href="https://github.com/AntCasson">
+                    <span className="sr-only">
+                        Link to GitHub page Anthony Casson
+                    </span>
+            </a>
             <div className="flex-header">
-                <button className="btn-count min" onClick={() => setSeconds(second => second - 1)}></button>
-                <h1>{seconds === 20 ? "Hyper" : `${seconds} sec`} Typer</h1>
-                <button className="btn-count plus" onClick={() => setSeconds(second => second + 1)}></button>                
+                <button 
+                        className="btn-count min" 
+                        onClick= {() => setSeconds(second => second - 1)}>
+                </button>
+                <h1>
+                    {seconds === 20 ? "Hyper" : `${seconds} sec`} 
+                    Typer
+                </h1>
+                <button 
+                    className="btn-count plus" 
+                    onClick={() => setSeconds(second => second + 1)}>
+                </button>                
             </div>
-            <p className="subtitle">Er zijn nog<span className="input text-light"disabled={isTimeRunning}
-                    onChange={secChange}> {isTimeRunning ? timeRemaining : seconds}  </span>seconden over.
+
+            <p className="subtitle">
+                    Er zijn nog 
+                        <span 
+                            className="input text-light" 
+                            disabled={isTimeRunning}> 
+                                {isTimeRunning ? timeRemaining : seconds}
+                        </span>
+                     seconden over.
             </p>
+
             <TextForTyping text={textToCheck}/>
+
             <textarea
                 ref={textBoxRef}
                 onChange={handleChange}
@@ -52,14 +74,16 @@ export default function App() {
                 placeholder="Druk op de start knop en gaan met die 🍌"
             />
             <div>
-                <button className="btn"
+                <button 
+                    className="btn"
                     onClick={startGame}
                     disabled={isTimeRunning}
                 >
                     Start
                 </button>
                 
-                <button className="btn"
+                <button 
+                    className="btn"
                     onClick={() => setTextToCheck(getRandomParagraph)}
                     disabled={isTimeRunning}
                 >
@@ -69,7 +93,11 @@ export default function App() {
             </div>
             
             <div className="score-time">
-               <p>Aantal woorden: <span className="text-light">{wordCount}</span></p>
+               <p>Aantal woorden: 
+                    <span className="text-light">
+                        {wordCount}
+                    </span>
+                </p>
             </div>
     
         </main>
